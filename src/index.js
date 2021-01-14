e9e4a030688326d9aa77ff758bb1a04cf1c6030b
Dipen Shah <dshah@trillium.group>
2021-01-14 11:45:21 -0500

    using props

--- src/index.js
+++ src/index.js
@@ -3,20 +3,35 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 
+const firstBook = {
+  author:'Dipen Shah',
+  title: 'The Intelligent Investor',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51fESaIGgVL._SX323_BO1,204,203,200_.jpg',
+};
 
-const author = 'Dipen Shah'
-const title = 'The Intelligent Investor';
-const img = 'https://images-na.ssl-images-amazon.com/images/I/51fESaIGgVL._SX323_BO1,204,203,200_.jpg'
+const secondBook = {
+  author:'Barack Obama',
+  title: 'A Promised Land',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
+};
+
 function BookList() {
   return (
     <section className='bookList'>
-      <Book job="developer" />
-      <Book title='random title' number={22}/>
+      <Book 
+      img={firstBook.img} 
+      title={firstBook.title} 
+      author = {firstBook.author} 
+      />
+      <Book  
+      img={secondBook.img} 
+      title={secondBook.title} 
+      author = {secondBook.author}
+      />
     </section>
   );
 }
-const Book = (props) => {
-  console.log(props);
+const Book = ({img, title, author}) => {
   return (<article className='book'>
     <img src={img} alt={title}></img>
     <h1>{title}</h1>
