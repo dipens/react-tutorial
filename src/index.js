414003a31e6637bddabb644ef1d98a1cc36ae735
Dipen Shah <dshah@trillium.group>
2021-01-14 21:27:49 -0500

    add onclick onmouseover event

--- src/index.js
+++ src/index.js
@@ -27,18 +27,27 @@ function BookList() {
     <section className='bookList'>
       {books.map((book) => {
         return (
-          <Book key={book.id} book={book}></Book>
+          <Book key={book.id} {...book}></Book>
         );
       })}
     </section>
   );
 }
-const Book = (props) => {
-  const {img, title, author} = props.book;
-  return (<article className='book'>
+const Book = ({img, title, author}) => {
+  const clickHandler = (e) => {
+    console.log(e.target);
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  }
+  return (<article className='book' onMouseOver={()=>{
+    console.log(title);
+  }}>
     <img src={img} alt={title}></img>
     <h1>{title}</h1>
     <h3>{author}</h3>
+    <button type="button" onClick={clickHandler}>Reference example</button>
+    <button type="button" onClick={() => {complexExample(author)}}>More complex example</button>
   </article>)
 }
 
