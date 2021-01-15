ff93947e0ca5b79a635f0a264e42afce8c4fdf3b
Dipen Shah <dshah@trillium.group>
2021-01-14 21:05:39 -0500

    added multiple books

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 import './index.css'
-
-
-const firstBook = {
-  author:'Dipen Shah',
-  title: 'The Intelligent Investor',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51fESaIGgVL._SX323_BO1,204,203,200_.jpg',
-};
-
-const secondBook = {
-  author:'Barack Obama',
-  title: 'A Promised Land',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
-};
+const books = [
+  {
+    id: 1,
+    author:'Dipen Shah',
+    title: 'The Intelligent Investor',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51fESaIGgVL._SX323_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 2,
+    author:'Barack Obama',
+    title: 'A Promised Land',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 3,
+    author:'George Orwell',
+    title: 'Nineteen Eighty Four',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51K84pomCRL._SX305_BO1,204,203,200_.jpg',
+  },
+];
 
 function BookList() {
   return (
     <section className='bookList'>
-      <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author = {firstBook.author} 
-      />
-      <Book  
-      img={secondBook.img} 
-      title={secondBook.title} 
-      author = {secondBook.author}
-      />
+      {books.map((book) => {
+        return (
+          <Book key={book.id} book={book}></Book>
+        );
+      })}
     </section>
   );
 }
-const Book = ({img, title, author}) => {
+const Book = (props) => {
+  const {img, title, author} = props.book;
   return (<article className='book'>
     <img src={img} alt={title}></img>
     <h1>{title}</h1>
